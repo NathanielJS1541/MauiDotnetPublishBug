8e8a94c85a8ad9debd2eb2f4ebed960b8631bd8d
Nathaniel Struselis <Nathaniel.Struselis@hotmail.co.uk>
2025-05-02 02:53:44 +0100

    Added MAUI sample app.

---
+++ src/MauiDotnetPublishBug.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiDotnetPublishBug
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/MauiDotnetPublishBug.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiDotnetPublishBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
